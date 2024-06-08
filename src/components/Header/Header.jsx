79f3cd3e5559e15cb38e942006d34ec0b2f1e971
hubnig <hubniggera@mail.ru>
2024-06-08 03:38:40 +0300

    add header timer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,49 @@
+import { useState, useEffect } from 'react'
 import './Header.scss'
-
 import logoSvg from './logo.svg'
 
 export default function Header() {
+	const [timer, setTimer] = useState({
+		days: 27,
+		hours: 21,
+		minutes: 0,
+		seconds: 0,
+	})
 
+	useEffect(() => {
+		const interval = setInterval(() => {
+			// Уменьшаем таймер на одну секунду
+			setTimer(prevTimer => {
+				const updatedTimer = { ...prevTimer }
+				if (updatedTimer.seconds > 0) {
+					updatedTimer.seconds--
+				} else {
+					if (updatedTimer.minutes > 0) {
+						updatedTimer.minutes--
+						updatedTimer.seconds = 59
+					} else {
+						if (updatedTimer.hours > 0) {
+							updatedTimer.hours--
+							updatedTimer.minutes = 59
+							updatedTimer.seconds = 59
+						} else {
+							if (updatedTimer.days > 0) {
+								updatedTimer.days--
+								updatedTimer.hours = 23
+								updatedTimer.minutes = 59
+								updatedTimer.seconds = 59
+							}
+						}
+					}
+				}
+				return updatedTimer
+			})
+		}, 1000)
+
+		return () => {
+			clearInterval(interval)
+		}
+	}, [])
 
 	return (
 		<div className='header'>
@@ -19,6 +59,12 @@ export default function Header() {
 				</a>
 				<div className='header__desc'>
 					Innovation
+					<br />
+					<span>
+						{' '}
+						до релиза приложения - {timer.days} дней {timer.hours} часов {timer.minutes}{' '}
+						минут {timer.seconds} секунд
+					</span>
 				</div>
 			</div>
 		</div>
